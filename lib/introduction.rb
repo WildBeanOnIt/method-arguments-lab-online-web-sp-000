032a249598746e338031673a588b290c1c2f4b0a
Julio Aguilera Jr <julioaguilera337@gmail.com>
2021-05-18 00:38:47 +0000

    Automatically backed up by Learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -3,5 +3,7 @@ def introduction(name)
 end
 
 def introduction_with_language(name, language)
-  "Hi, my name is #{name} and i am learning to program in #{language}."
+    puts "Hi, my name is #{name} and i am learning to program in #{language}."
 end
+
+introduction_with_language("Den", "Ember.js")
